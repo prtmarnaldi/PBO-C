0123aa5124aff5a55412369d5f0ab8a56b2990d2
pratamarenaldi <sigitpratamarenaldi195@gmail.com>
2025-11-13 16:46:23 +0700

    LuasLingkaran.php

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,29 +1,10 @@
 <?php
-
-namespace App\Math;
-
-class LuasLingkaran {
-
+class LuasLingkaran{
     public const phi = 3.14;
-    protected int $jari;
-    
-    public function __construct($isiJari = 1) {
-        $this->jari = $isiJari; //10
-    }
-
-    public function tampil($nama = 'ban') {
-        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
-        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
-    }
-
-    public static function testing() {
-        echo "<br/>";
-        echo "ini dari static";
-    }
-
-    public function __destruct() {
-        echo "<br/>";
-        echo "udah ah cape";
-    }
+    public int $jari;
 }
+$Lingkaran = new LuasLingkaran();
+$Lingkaran -> jari = 7;
 
+$Rumus = LuasLingkaran : : phi * $Lingkaran -> jari * $Lingkaran -> jari;
+ echo "Hasilnya Adalah;". $Rumus;
